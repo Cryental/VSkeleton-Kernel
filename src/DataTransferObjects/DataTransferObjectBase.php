7f6f08ee0e5a6f6d9d3006c087ff5c5c366e4bab
Cryental <contact@cryental.dev>
2024-04-20 17:52:43 +0600

    updated for laravel 11

--- src/DataTransferObjects/DataTransferObjectBase.php
+++ src/DataTransferObjects/DataTransferObjectBase.php
@@ -2,11 +2,12 @@
 
 namespace Volistx\FrameworkKernel\DataTransferObjects;
 
+use InvalidArgumentException;
 use ReflectionClass;
 use ReflectionProperty;
 use stdClass;
 
-class DataTransferObjectBase
+abstract class DataTransferObjectBase
 {
     protected $entity;
 
@@ -16,11 +17,21 @@ class DataTransferObjectBase
 
         $class = new ReflectionClass(static::class);
 
-        $parameters = $entity instanceof stdClass ? get_object_vars($entity) : (is_array($entity) ? $entity : $entity->toArray());
+        if ($entity instanceof stdClass) {
+            $parameters = get_object_vars($entity);
+        } elseif (is_array($entity)) {
+            $parameters = $entity;
+        } elseif (is_object($entity) && method_exists($entity, 'toArray')) {
+            $parameters = $entity->toArray();
+        } else {
+            throw new InvalidArgumentException("Constructor parameter must be an array, a stdClass object or an object with a toArray method.");
+        }
 
         foreach ($class->getProperties(ReflectionProperty::IS_PUBLIC) as $reflectionProperty) {
             $property = $reflectionProperty->getName();
-            $this->{$property} = $parameters[$property];
+            if (array_key_exists($property, $parameters)) {
+                $this->{$property} = $parameters[$property];
+            }
         }
     }
 }
